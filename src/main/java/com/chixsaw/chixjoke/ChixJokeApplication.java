ca7fe17000a555a5c023e3f3bda2776fc22b44b4
RaphaelFrey <raphael.marius.frey@gmail.com>
2022-11-25 12:46:57 +0100

    Inital comitt

---
+++ src/main/java/com/chixsaw/chixjoke/ChixJokeApplication.java
@@ -0,0 +1,13 @@
+package com.chixsaw.chixjoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChixJokeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChixJokeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chixsaw/chixjoke/ChixJokeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chixsaw.chixjoke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChixJokeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
