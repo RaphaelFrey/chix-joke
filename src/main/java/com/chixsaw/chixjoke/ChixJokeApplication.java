4fe60cb3c2bd829fd25cb289e112bc3dc80617b6
RaphaelFrey <raphael.marius.frey@gmail.com>
2022-11-25 13:17:48 +0100

    Created index.html and wired it up. closes #5 closes #6

--- src/main/java/com/chixsaw/chixjoke/ChixJokeApplication.java
+++ src/main/java/com/chixsaw/chixjoke/ChixJokeApplication.java
@@ -2,8 +2,9 @@ package com.chixsaw.chixjoke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ChixJokeApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/chixsaw/chixjoke/services/JokeServiceImpl.java
+++ src/main/java/com/chixsaw/chixjoke/services/JokeServiceImpl.java
@@ -8,9 +8,15 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
     @Override
     public String getJoke() {
-        ChuckNorrisQuotes chuckNorrisQuotes = new ChuckNorrisQuotes();
         return chuckNorrisQuotes.getRandomQuote();
     }
 }
